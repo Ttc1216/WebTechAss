a6a445a7895c309545ff69c8c40381a18f2f1b96
Jiani0327 <bcs21090012@student.ucts.edu.my>
2023-03-30 10:35:40 +0800

    hi

--- WebTechAss/wwwroot/js/site.js
+++ WebTechAss/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnlogin-popup');
+const iconClose = document.querySelector('.icon-close');
+
+registerLink.addEventListener('click', () => { wrapper.classList.add('active'); });
+loginLink.addEventListener('click', () => { wrapper.classList.remove('active'); });
+btnPopup.addEventListener('click', () => { wrapper.classList.add('active-popup'); });
+iconClose.addEventListener('click', () => { wrapper.classList.remove('active-popup'); });
