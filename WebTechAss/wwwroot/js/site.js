22c8c5fc5b8ddc029f625f110afc9b8d62127101
Jiani0327 <bcs21090012@student.ucts.edu.my>
2023-04-04 10:35:25 +0800

    new

--- WebTechAss/wwwroot/js/site.js
+++ WebTechAss/wwwroot/js/site.js
@@ -2,13 +2,37 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-const wrapper = document.querySelector('.wrapper');
-const loginLink = document.querySelector('.login-link');
-const registerLink = document.querySelector('.register-link');
-const btnPopup = document.querySelector('.btnlogin-popup');
-const iconClose = document.querySelector('.icon-close');
+//const wrapper = document.querySelector('.wrapper');
+//const loginLink = document.querySelector('.login-link');
+//const registerLink = document.querySelector('.register-link');
+//const btnPopup = document.querySelector('.btnlogin-popup');
+//const iconClose = document.querySelector('.icon-close');
 
-registerLink.addEventListener('click', () => { wrapper.classList.add('active'); });
-loginLink.addEventListener('click', () => { wrapper.classList.remove('active'); });
-btnPopup.addEventListener('click', () => { wrapper.classList.add('active-popup'); });
-iconClose.addEventListener('click', () => { wrapper.classList.remove('active-popup'); });
+//registerLink.addEventListener('click', () => { wrapper.classList.add('active'); });
+//loginLink.addEventListener('click', () => { wrapper.classList.remove('active'); });
+//btnPopup.addEventListener('click', () => { wrapper.classList.add('active-popup'); });
+//iconClose.addEventListener('click', () => { wrapper.classList.remove('active-popup'); });
+new Swiper('.testimonials-slider', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 40
+        },
+
+        1200: {
+            slidesPerView: 3,
+        }
+    }
+});
