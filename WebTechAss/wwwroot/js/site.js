1b1dfa86c2f7acb18ddd655c3403cdb5d9431062
Jiani0327 <bcs21090012@student.ucts.edu.my>
2023-04-17 15:53:54 +0800

    17/04

--- WebTechAss/wwwroot/js/site.js
+++ WebTechAss/wwwroot/js/site.js
@@ -23,3 +23,21 @@ function toggleAccordion() {
     }
 }
 items.forEach((item) => item.addEventListener('click', toggleAccordion));
+
+/**
+   * Testimonials slider
+   */
+new Swiper('.testimonials-slider', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true
+    }
+});
