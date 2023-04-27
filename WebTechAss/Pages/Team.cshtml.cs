2899f081288ee03f3b1a52a2a1acf4c65e558176
Jiani0327 <bcs21090012@student.ucts.edu.my>
2023-04-27 10:27:11 +0800

    27/04

---
+++ WebTechAss/Pages/Team.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebTechAss.Pages
+{
+    public class TeamModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
