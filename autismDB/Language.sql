4deb7f8c51bc3b712ac2635997f8dc987ba2a2da
Ttc1216 <tangtiewchung01@gmail.com>
2023-05-25 01:03:16 +0800

    Employer table

--- autismDB/Language.sql
+++ autismDB/Language.sql
@@ -1,7 +1,9 @@
 ﻿CREATE TABLE [dbo].[Language]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [Name] NVARCHAR(50) NOT NULL,
+    [Name] NVARCHAR(50) NOT NULL, 
+    [English] NVARCHAR(50) NOT NULL, 
+    [Chinese] NVARCHAR(50) NOT NULL, 
+    [Malay] NVARCHAR(50) NOT NULL,
 )
 
---English,Chinese,Malay
