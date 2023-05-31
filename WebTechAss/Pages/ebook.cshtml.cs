b672aa295817c237e2313e8f5512ace398375cef
LING JIA NI <BCS21090012@student.ucts.edu.my>
2023-05-31 16:16:45 +0800

    31/05

---
+++ WebTechAss/Pages/ebook.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebTechAss.Pages
+{
+    public class ebbokModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ admin panel/Pages/employment.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace admin_panel.Pages
+{
+    public class employmentModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
