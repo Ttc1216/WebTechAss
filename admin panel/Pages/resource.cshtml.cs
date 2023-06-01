20aac204f078cc5c6881b79a5d31d34846c8a634
LING JIA NI <BCS21090012@student.ucts.edu.my>
2023-06-01 15:35:43 +0800

    updated

---
+++ admin panel/Pages/resource.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace admin_panel.Pages
+{
+    public class resourceModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
