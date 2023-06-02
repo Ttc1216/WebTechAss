0fed117e91e0a2efea788fb3aab5042b6eda5175
LING JIA NI <BCS21090012@student.ucts.edu.my>
2023-06-02 11:55:08 +0800

    update

---
+++ admin panel/Pages/task.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace admin_panel.Pages
+{
+    public class taskModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
